bf15adf29ecf53736de9e359d75114b60332109a
Florin Mihalache <florinrm@users.noreply.github.com>
2017-11-23 11:54:11 +0200

    Add files via upload

---
+++ Lab8/lab-08-tasks/4-5-max-c-calls/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// unsigned int get_max(unsigned int *arr, unsigned int len); de la 4
+unsigned int get_max(unsigned int *arr, unsigned int len, unsigned int *pos);
+
+int main(void)
+{
+	unsigned int arr[] = { 19, 7, 129, 87, 54, 218, 67, 12, 19, 99 };
+	unsigned int max;
+
+	unsigned int pos;
+
+	max = get_max(arr, 10, &pos);
+
+	printf("max: %u\n", max);
+	printf("position: %u\n", pos);
+
+	return 0;
+}
---
+++ Lab8/lab-08-tasks/8-max-c-calls-x64/max.asm
@@ -0,0 +1,26 @@
+section .text
+
+global get_max
+
+get_max:
+	push rbp
+	mov rbp, rsp
+
+	; [ebp+8] is array pointer
+	; [ebp+12] is array length
+
+	mov rbx, rdi
+	mov rcx, rsi
+	xor rax, rax
+
+compare:
+	cmp eax, [rbx+rcx*4 - 4]
+	jge check_end
+	mov eax, [rbx+rcx*4 - 4]
+	dec rcx
+	mov [rdx], rcx
+check_end:
+	loopnz compare
+
+	leave
+	ret
